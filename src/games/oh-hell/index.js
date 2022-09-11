25a046ea7b292e2c343332f03d07929b18c4befd
Mario T. Lanza <mlanza@doesideas.com>
2022-09-11 12:03:21 -0400

    tweak

--- src/games/oh-hell/index.js
+++ src/games/oh-hell/index.js
@@ -6,7 +6,19 @@ fetch("./data/events.json").
   then(function(resp){
     return resp.json();
   }).
-  then(g.aggregate(oh.ohHell(["Ava", "Zoe", "Jennabel", "Mario"], {}), _, g.inspect)).
+  then(g.aggregate(oh.ohHell([{
+    username: "brielle",
+    id: "8cb76dc4-4338-42d4-a324-b61fcb889bd1"
+  }, {
+    username: "jazz",
+    id: "4c2e10da-a868-4098-aa0d-030644b4e4d7"
+  }, {
+    username: "todd",
+    id: "c8619345-0c1a-44c4-bdfe-e6e1de11c6bd"
+  }, {
+    username: "mario",
+    id: "5e6b12f5-f24c-4fd3-8812-f537778dc5c2"
+  }], {}), _, g.inspect)).
   then(_.invoke(_, [], null)). //no new commands
   then(_.see("aggregate"));
 

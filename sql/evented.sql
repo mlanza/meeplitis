6f577d93ed659e8f4a0c3ada04710ac282e4b646
Mario T. Lanza <mlanza@doesideas.com>
2022-09-15 21:27:45 -0400

    left join

--- sql/evented.sql
+++ sql/evented.sql
@@ -7,7 +7,7 @@ begin
 return (select json_agg(json_build_object('id', id, 'type', type, 'details', details, 'seat', seat)) as evented
   from (select e.id, e.type, e.details, s.seat
         from events e
-        join seats s on s.table_id = e.table_id and s.id = e.seat_id
+        left join seats s on s.table_id = e.table_id and s.id = e.seat_id
         where e.table_id = _table_id
         order by e.seq) as e);
 

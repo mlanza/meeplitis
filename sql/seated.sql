afa301149f8ebd8783438151a7ad7e9150636842
Mario T. Lanza <mlanza@doesideas.com>
2022-09-15 21:32:50 -0400

    tweak

--- sql/seated.sql
+++ sql/seated.sql
@@ -4,7 +4,7 @@ language plpgsql
 as $$
 begin
 
-return (select json_agg(json_build_object('player_id', s.player_id, 'username', s.username, 'seat', s.id)) as seated
+return (select json_agg(json_build_object('player_id', s.player_id, 'username', s.username, 'seat_id', s.id)) as seated
   from (select s.player_id, p.username, s.id
   from seats s
   join profiles p on p.id = s.player_id

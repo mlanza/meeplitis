eb21bedc18154760657d0aec3d446b9207b63d75
Mario T. Lanza <mlanza@doesideas.com>
2022-09-25 13:45:31 -0400

    dynamically choose which fn to execute

--- sql/simulate.sql
+++ sql/simulate.sql
@@ -6,13 +6,32 @@ _result jsonb;
 _seated jsonb;
 _events jsonb;
 _config jsonb;
+_sql text;
 begin
 
-_seated := (select seated(_table_id));
-_events := (select evented from evented(_table_id));
-_config := (select config from tables where id = _table_id);
-_fn := (select fn from tables where id = _table_id limit 1);
-_result := (select case _fn when 'ohhell' then ohhell(_seated, _config, _events, _commands, array[_seat]) else null end);
+select seated(_table_id)
+into _seated;
+
+select evented
+from evented(_table_id)
+into _events;
+
+select config
+from tables
+where id = _table_id
+into _config;
+
+select fn
+from tables
+where id = _table_id
+into _fn;
+
+--_result := (select case _fn when 'ohhell' then ohhell(_seated, _config, _events, _commands, array[_seat]) else null end);
+-- dynamically choose which fn to execute
+select 'select ' || _fn || '(''' || _seated || ''', ''' || _config || ''', ''' || _events || ''', ''' || _commands || ''', array[' || _seat || '])'
+into _sql;
+
+execute _sql into _result;
 
 return _result;
 end;

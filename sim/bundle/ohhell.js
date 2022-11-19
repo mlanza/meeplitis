bd745f6bd55dbe34bd3fd901edbea518122ae92a
Mario T. Lanza <mlanza@doesideas.com>
2022-11-19 15:38:14 -0500

    adjust layouts

--- sim/bundle/ohhell.js
+++ sim/bundle/ohhell.js
@@ -1,5 +1,5 @@
 import * as g from "../../src/lib/game.js";
-import ohHell from "../../src/games/oh-hell/lib/index.js";
+import ohHell from "../../src/games/oh-hell/core.js";
 
 export function simulate(seats, config, events, commands, seen){
   return g.simulate(ohHell(seats, config), events, commands, seen);

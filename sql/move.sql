bbb4b3aef851b5e9d816f2cb8c30bb3172a0a4df
Mario T. Lanza <mlanza@doesideas.com>
2022-12-03 16:22:45 -0500

    trim notices

--- sql/move.sql
+++ sql/move.sql
@@ -6,6 +6,7 @@ language plpgsql
 as $$
 declare
 _count int;
+_recipients int;
 _simulated jsonb;
 _up smallint[];
 _seat int;
@@ -18,7 +19,7 @@ and player_id = auth.uid()
 into _seat;
 
 if _seat is null then
-  raise exception 'Only seated players may issue moves';
+  raise exception 'only players may issue moves';
 end if;
 
 select count(*)
@@ -26,7 +27,7 @@ from jsonb_array_elements_text(_commands)
 into _count;
 
 if _count = 0 then
-  raise exception 'No move issued';
+  raise exception 'must provide command(s)';
 end if;
 
 raise log '$ seat % at table `%` moves issuing % command(s): %', _seat, _table_id, _count, _commands;
@@ -38,14 +39,68 @@ update tables
 set up = _up
 where tables.id = _table_id;
 
-insert into jobs(type, details)
-values ('up:notice', ('{"table_id": "' || _table_id || '", "seats": ' || (_simulated->'notify')::jsonb || '}')::jsonb);
+select jsonb_array_length((_simulated->'notify')::jsonb)
+into _recipients;
+
+if _recipients > 0 then
+  insert into jobs(type, details)
+  values ('up:notice', ('{"table_id": "' || _table_id || '", "seats": ' || (_simulated->'notify')::jsonb || '}')::jsonb);
+end if;
 
 return query
 insert into events (table_id, type, details, seat_id)
 select s.table_id, e.type, e.details, s.id as seat_id
 from addable_events(_simulated->'added') e
-join seats s on s.table_id = _table_id and s.seat = e.seat
+left join seats s on s.table_id = _table_id and s.seat = e.seat
 returning events.id, events.table_id, events.type, events.seat_id;
 
-end; $$
+end;
+$$;
+
+create or replace function move(_table_id varchar, _commands jsonb, _seat int)
+returns table(id varchar, table_id varchar, type varchar, seat_id varchar)
+language plpgsql
+as $$
+declare
+_count int;
+_recipients int;
+_simulated jsonb;
+_up smallint[];
+begin
+
+select count(*)
+from jsonb_array_elements_text(_commands)
+into _count;
+
+raise log '$ here';
+
+if _count = 0 then
+  raise exception 'must provide command(s)';
+end if;
+
+raise log '$ seat % at table `%` moves issuing % command(s): %', _seat, _table_id, _count, _commands;
+
+_simulated := (select simulate(_table_id, _commands, _seat));
+_up := (select array_agg(value::smallint)::smallint[] from jsonb_array_elements(_simulated->'up'));
+
+update tables
+set up = _up
+where tables.id = _table_id;
+
+select jsonb_array_length((_simulated->'notify')::jsonb)
+into _recipients;
+
+if _recipients > 0 then
+  insert into jobs(type, details)
+  values ('up:notice', ('{"table_id": "' || _table_id || '", "seats": ' || (_simulated->'notify')::jsonb || '}')::jsonb);
+end if;
+
+return query
+insert into events (table_id, type, details, seat_id)
+select _table_id as table_id, e.type, e.details, s.id as seat_id
+from addable_events(_simulated->'added') e
+left join seats s on s.table_id = _table_id and s.seat = e.seat
+returning events.id, events.table_id, events.type, events.seat_id;
+
+end;
+$$;

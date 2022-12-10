b686fe516678409c4ae4347e7be0ab71f19ce1b4
Mario T. Lanza <mlanza@doesideas.com>
2022-12-10 13:26:27 -0500

    update email

--- src/profiles/edit/index.js
+++ src/profiles/edit/index.js
@@ -8,16 +8,24 @@ import {session} from "/lib/session.js";
 const username = dom.sel1("#username"),
       headline = dom.sel1("#headline"),
       description = dom.sel1("#description"),
+      email = dom.sel1("#email"),
       form = dom.sel1("form"),
       pattern = new RegExp(dom.attr(username, "pattern"));
 
 form.addEventListener("submit", process);
 
+if (!session?.userId) {
+  location.href = "/signin";
+}
+
 const { data: [profile], error } = await supabase
   .from('profiles')
   .select('*')
-  .eq('id', session.userId);
+  .eq('id', session?.userId);
 
+const {data: {user}} = await supabase.auth.getUser();
+
+_.maybe(user, _.see("user"), _.get(_, "email"), dom.value(email, _));
 _.maybe(profile, _.get(_, "username"), _.str(_, " | ", "Your Move"), dom.text(dom.sel1("head title"), _));
 _.maybe(profile, _.get(_, "username"), dom.text(dom.sel1(".banner h1"), _));
 _.maybe(profile, _.get(_, "username"), function(username){
@@ -37,18 +45,26 @@ if (!profile) {
 async function process(e) {
   e.preventDefault();
   if (username.validity.valid && headline.validity.valid) {
-    const { data, error } = await supabase
-      .from('profiles')
-      .upsert({
-        id: session.userId,
-        username: dom.value(username),
-        headline: dom.value(headline) || null,
-        description: dom.value(description) || null
-      })
-      .select();
+    (async function(){
+      const { data, error } = await supabase
+        .from('profiles')
+        .upsert({
+          id: session.userId,
+          username: dom.value(username),
+          headline: dom.value(headline) || null,
+          description: dom.value(description) || null
+        })
+        .select();
       location.reload();
-  } else {
-    _.log("fail");
+    })();
+  }
+  if (email.validity.valid && user.email !== dom.value(email)) {
+    (async function(){
+      const { data, error } = await supabase.auth.updateUser({
+        email:  dom.value(email)
+      });
+      alert("You will receive a notification about your email address being changed.  Once you've confirmed and logged in again the update will take effect.");
+    })();
   }
   return false; //returning false prevents default behavior
 }

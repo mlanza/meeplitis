81d6216b3f04730802715ec6c1c2177de60483ad
Mario T. Lanza <mlanza@doesideas.com>
2023-04-10 14:03:27 -0400

    revise keys

--- src/games/mexica/table/index.js
+++ src/games/mexica/table/index.js
@@ -91,7 +91,21 @@ const [seated, seat] = await Promise.all([
   getSeat(tableId, session)
 ]);
 
-//dom.append(el, kbd({class: "named"}, "esc"));
+dom.append(el,
+  _.doto(
+    span(
+    _.doto(kbd({class: "symbol"}), dom.html(_, "&#8593")),
+    " ",
+    _.doto(kbd({class: "symbol"}), dom.html(_, "&#8595")),
+    " ",
+    _.doto(kbd({class: "symbol"}), dom.html(_, "&#8592")),
+    " ",
+    _.doto(kbd({class: "symbol"}), dom.html(_, "&#8594")),
+    " ",
+    kbd({class: "named wide"}, "return"),
+    " ",
+    _.doto(kbd({class: "symbol"}), dom.html(_, "&#8593"))),
+    dom.addClass(_, "keys")));
 
 function resources(title, resource, _attrs, supply){
   const attrs = Object.assign({orientation: "vertical", size: 1}, _attrs);

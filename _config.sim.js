773ee41b53f3b46420f689431dea34cd9bef1d8a
Mario T. Lanza <mlanza@doesideas.com>
2023-04-19 17:28:03 -0400

    add minification of bundles for postgres fn

--- _config.sim.js
+++ _config.sim.js
@@ -1,15 +1,17 @@
 import lume from "lume/mod.ts";
+import terser from "lume/plugins/terser.ts";
 
 function simulation(page){
   page.dest.ext = ".js"
   const named = page.src.path.replace("/","");
   const contents = page.content.split("\n").filter(function(line){
     return !line.startsWith("//") && !line.startsWith("export");
-  });
+  }).join("\n").replace("export{simulate1 as simulate};", "");
+
   page.content = `create or replace function ${named}(_seats jsonb, _config jsonb, _events jsonb, _commands jsonb, _seen int[], _snapshot jsonb)
 returns jsonb as $$
   const simulate1 = (function(){
-    ${contents.join("\n")}
+    ${contents}
     return simulate1;
   })();
 return simulate1(_seats, _config || {}, _events || [], _commands || [], _seen || [], _snapshot);
@@ -23,6 +25,11 @@ export default lume({
   src: "./sim/src",
   dest: "./sim/dist",
   prettyUrls: false
-})
+}).
+  use(terser({
+    options: {
+      module: false
+    }
+  }))
   .loadPages([".js"])
   .process([".js"], simulation);

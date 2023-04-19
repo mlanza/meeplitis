d9b03d97ef8b08c4c578ec652a128f10b7db19c7
Mario T. Lanza <mlanza@doesideas.com>
2023-04-19 19:54:45 -0400

    deploy edge mexica

---
+++ supabase/functions/mexica/index.ts
@@ -0,0 +1,20 @@
+// @ts-nocheck
+import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
+import {comp} from "https://yourmove.cc/lib/atomic/core.js";
+import * as g from "https://yourmove.cc/lib/game.js";
+import mexica from "https://yourmove.cc/games/mexica/core.js";
+
+const simulate = comp(g.effects, g.simulate(mexica));
+
+console.log("The mind of Mexica!");
+
+serve(async (req) => {
+  const { seats, config, events, commands, seen, snapshot } = await req.json();
+  const data = simulate(seats, config || {}, events || [], commands || [], seen || [], snapshot);
+  return new Response(
+    JSON.stringify(data),
+    { headers: { "Content-Type": "application/json" } },
+  )
+});
+
+// To invoke: curl -i --location --request POST 'https://miwfiwpgvfhggfnqtfso.functions.supabase.co/mexica' --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODUwMzIzMywiZXhwIjoxOTU0MDc5MjMzfQ.i1l7NNGYF7mChifi8X-Cn_tis-us1Zq1ntyVW-Amdf8' --header 'Content-Type: application/json' --data '{"seats": [{}, {}, {}], "config": {}, "events": [{"type": "started"},{"type": "dealt-capulli", "details": {"capulli": [[{"at": null,"size": 13},{"at": null,"size": 11},{"at": null,"size": 9},{"at": null,"size": 6},{"at": null,"size": 5},{"at": null,"size": 4},{"at": null,"size": 4},{"at": null,"size": 3}],[{"at": null,"size": 12},{"at": null,"size": 10},{"at": null,"size": 8},{"at": null,"size": 7},{"at": null,"size": 6},{"at": null,"size": 5},{"at": null,"size": 3}]]}}], "commands": [], "seen": [1], "snapshot": null}'

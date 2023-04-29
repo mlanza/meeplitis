b25fc1c32f1918d2c106804e69f938ac8a545414
Mario T. Lanza <mlanza@doesideas.com>
2023-04-29 13:18:45 -0400

    call game mind

--- fly/NOTES.md
+++ fly/NOTES.md
@@ -2,3 +2,7 @@
 
 * `flyctl deploy`
 * `flyctl open`
+* `deno run --allow-all ./main.ts -p 8090`
+* http://localhost:8080/debug/?id=bIQdLcjcTxK&options=nomonitor,local
+* `curl -i --location --request POST 'https://yourmove.fly.dev/mind/mexica' --header 'Content-Type: application/json' --data '{"seats": [{}, {}, {}], "config": {}, "events": [{"type": "started"},{"type": "dealt-capulli", "details": {"capulli": [[{"at": null,"size": 13},{"at": null,"size": 11},{"at": null,"size": 9},{"at": null,"size": 6},{"at": null,"size": 5},{"at": null,"size": 4},{"at": null,"size": 4},{"at": null,"size": 3}],[{"at": null,"size": 12},{"at": null,"size": 10},{"at": null,"size": 8},{"at": null,"size": 7},{"at": null,"size": 6},{"at": null,"size": 5},{"at": null,"size": 3}]]}}], "commands": [], "seen": [1], "snapshot": null}'
+`
--- fly/main.ts
+++ fly/main.ts
@@ -2,29 +2,45 @@ import {
   app,
   get,
   post,
+  options,
   redirect,
   contentType,
 } from "https://denopkg.com/syumai/dinatra/mod.ts";
 import {comp} from "https://yourmove.cc/lib/atomic/core.js";
 import * as g from "https://yourmove.cc/lib/game.js";
 
-console.log("The Game Mind lives!");
+const headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, OPTIONS",
+  "access-control-allow-credentials": true,
+  "access-control-allow-headers": "Origin, X-Requested-With, Content-Type, Accept",
+  "access-control-max-age": 10,
+  "content-type": "application/json"
+};
 
 app(
-  post("/mind/:game", function({params: {game, seats, config, events, commands, seen, snapshot}}){
+  post("/mind/:game", function(req){
+    const {params} = req;
+    const {game, seats, config, events, commands, seen, snapshot} = params;
+    //console.log("req", JSON.stringify(req));
     return import(`https://yourmove.cc/games/${game}/core.js`).then(function({make}){
       const simulate = comp(g.effects, g.simulate(make));
       const results = simulate(seats, config || {}, events || [], commands || [], seen || [], snapshot || null)
       return [
         200,
-        contentType("json"),
+        headers,
         JSON.stringify(results)];
     });
   }),
-  get("/", () => "<h1>The Game Mind lives!</h1>")
+  options("/mind/:game", function(){
+    return [200, headers, ""];
+  }),
+  get("/", () => "<h1>The Game Mind lives!</h1>"),
+  get("/info", () => [
+    200,
+    contentType("json"),
+    JSON.stringify({ app: "mind", version: "1.0.0" })
+  ])
 );
 
-
-// To invoke: curl -i --location --request POST 'https://yourmove.fly.dev/mind/mexica' --header 'Content-Type: application/json' --data '{"seats": [{}, {}, {}], "config": {}, "events": [{"type": "started"},{"type": "dealt-capulli", "details": {"capulli": [[{"at": null,"size": 13},{"at": null,"size": 11},{"at": null,"size": 9},{"at": null,"size": 6},{"at": null,"size": 5},{"at": null,"size": 4},{"at": null,"size": 4},{"at": null,"size": 3}],[{"at": null,"size": 12},{"at": null,"size": 10},{"at": null,"size": 8},{"at": null,"size": 7},{"at": null,"size": 6},{"at": null,"size": 5},{"at": null,"size": 3}]]}}], "commands": [], "seen": [1], "snapshot": null}'
-//curl --header "Content-Type: application/json" --request POST --data '{"seats": [{}, {}, {}], "events":[], "commands": [{"type": "start"}], "seen": [0]}' https://yourmove.fly.dev/mexica
-//curl --header "Content-Type: application/json" --request POST --data '{"seats": [{}, {}, {}], "events":[], "commands": [{"type": "start"}], "seen": [0]}' https://yourmove.fly.dev/mexica
+console.log("The Game Mind lives!");

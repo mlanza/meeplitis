66e6dd5912b63000dd27127d816e98e4bdda8b3f
Mario T. Lanza <mlanza@doesideas.com>
2023-05-14 12:21:23 -0400

    tweak agg

--- aggregate.js
+++ aggregate.js
@@ -5,16 +5,20 @@ import g from "./src/lib/game_.js";
 import supabase from "./src/lib/supabase.js";
 
 const tableId = Deno.args[0],
-      limit   = Deno.args[1] || 1000;
+      limit   = Deno.args[1] || 200;
 
-const [tables, seated, evented] = await Promise.all([
+const [_tables, _seated, _events] = await Promise.all([
   supabase.from("tables").select('*,game_id(slug)').eq("id", tableId),
   supabase.rpc('seated', {_table_id: tableId}),
-  supabase.rpc('evented', {_table_id: tableId})
+  supabase.from("events").select('*').eq("table_id", tableId).order("seq").limit(limit)
 ]);
 
-const table = tables.data[0],
-      events = _.chain(evented.data || [], _.take(limit, _), _.toArray),
+const table = _tables.data[0],
+      seated = _.mapa(_.get(_, "seat_id"), _seated.data),
+      events = _.mapa(function({id, type, details, seat_id}){
+        const seat = _.maybe(seat_id, _.indexOf(seated, _));
+        return _.compact({id, type, details, seat});
+      }, _events.data),
       seats = _.toArray(_.repeat(_.count(seated.data) || 4, {})),
       seen = _.toArray(_.range(0, _.count(seats))),
       commands = [],

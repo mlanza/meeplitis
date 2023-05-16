816ec3b62fae94c3af463b2666dc02bfdaa3796a
Mario T. Lanza <mlanza@doesideas.com>
2023-05-16 08:33:01 -0400

    add effects flag to step

--- src/debug/step/index.js
+++ src/debug/step/index.js
@@ -4,12 +4,14 @@ import supabase from "/lib/supabase.js";
 
 const params = new URLSearchParams(document.location.search),
       game = params.get('game'),
-      payload = await fetch(`./${game}.json`).then(resp => resp.json());
+      effects = params.get('effects') == 1,
+      payload = await fetch(`./${game}.json`).then(resp => resp.json()),
+      op = effects ? g.effects : _.identity;
 
 _.log("payload", game, payload);
 
 const {make} = await import(`/games/${game}/core.js`);
-const simulate = g.simulate(make);
+const simulate = _.comp(op, g.simulate(make));
 const result = simulate(payload);
 
 _.log("result", game, result);

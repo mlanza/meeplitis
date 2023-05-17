226fa4a66e2a993edd0f2fcbf1cfee12addd3e1a
Mario T. Lanza <mlanza@doesideas.com>
2023-05-17 17:43:29 -0400

    update simulate

--- fly/game.ts
+++ fly/game.ts
@@ -1,10 +1,10 @@
 // @ts-nocheck
-import * as g from "https://yourmove.cc/lib/game.js";
-import * as _ from "https://yourmove.cc/lib/atomic/core.js";
+import * as g from "game";
+import * as _ from "atomic/core";
 
 self.onmessage = async function(e){
   const {game, seats, config, events, commands, seen, snapshot} = e.data;
-  const {make} = await import(`https://yourmove.cc/games/${game}/core.js`);
+  const {make} = await import(game);
   const simulate = _.comp(g.effects, g.simulate(make));
   self.postMessage(_.chain(simulate(seats, config, events, commands, seen, snapshot), x => _.update(x, "moves", _.toArray)));
   self.close();

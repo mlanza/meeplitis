95efd12d346a35e074119b4c2d09ecd3d10795e2
Mario T. Lanza <mlanza@doesideas.com>
2023-07-04 09:06:17 -0400

    implement ui

--- src/games/mexica/table/index.js
+++ src/games/mexica/table/index.js
@@ -478,14 +478,16 @@ function marking(options, which, target){
 
 const mark = _.partial(marking, ["pilli", "bridge"]);
 
-$.on(el, "keydown", 'body[data-tense="present"][data-ready="true"]', function(e){
+const ctx = 'body[data-tense="present"][data-ready="true"]'; //ensure actionable context
+
+$.on(el, "keydown", ctx, function(e){
   if (e.key === "Escape") { //cancel a command in progress
     sh.dispatch($wip, null);
     dom.attr(el, "data-show-error", false);
   }
 });
 
-$.on(el, "mouseover", 'body[data-tense="present"][data-ready="true"] div[data-spot]', function(e){
+$.on(el, "mouseover", `${ctx} div[data-spot]`, function(e){
   const bridge = dom.sel1(".contents > [data-piece='bridge']", this),
         pilli  = dom.sel1(".contents > [data-piece='pilli']", this),
         you    = _.maybe(pilli, dom.attr(_, "data-seat"), parseInt, _.eq(seat, _)),
@@ -493,69 +495,69 @@ $.on(el, "mouseover", 'body[data-tense="present"][data-ready="true"] div[data-sp
   mark(id, this);
 });
 
-$.on(el, "click", 'body[data-tense="present"][data-ready="true"] #pilli[data-spot]', function(e){
+$.on(el, "click", `${ctx} #pilli[data-spot]`, function(e){
   const type = "move",
         from = getAttr(this, "data-spot");
   sh.dispatch($wip, {type, details: {from}});
 });
 
-$.on(el, "click", 'body[data-tense="present"][data-ready="true"] .zone.yours .area div.temples:not([data-remaining="0"])', function(e){
+$.on(el, "click", `${ctx} .zone.yours .area div.temples:not([data-remaining="0"])`, function(e){
   const type = "build-temple",
         size = parseInt(getAttr(this, "data-size"));
   sh.dispatch($wip, {type, details: {size}});
 });
 
-$.on(el, "click", 'body[data-tense="present"][data-ready="true"][data-command-type="build-temple"] div[data-spot]', function(e){
+$.on(el, "click", `${ctx}[data-command-type="build-temple"] div[data-spot]`, function(e){
   const type = "build-temple",
         at   = getAttr(this, "data-spot"),
         level = parseInt(getAttr(this, "data-command-size"));
   sh.dispatch($story, {type, details: {level, at}});
 });
 
-$.on(el, "click", 'body[data-tense="present"][data-ready="true"][data-command-type="relocate-bridge"][data-command-from] [data-spot]', function(e){
+$.on(el, "click", `${ctx}[data-command-type="relocate-bridge"][data-command-from] [data-spot]`, function(e){
   const type = "relocate-bridge",
         from = getAttr(this, "data-command-from"),
         to   = getAttr(this, "data-spot");
   sh.dispatch($story, {type, details: {from, to}});
 });
 
-$.on(el, "click", 'body[data-tense="present"][data-ready="true"] #bridge[data-spot]', function(e){
+$.on(el, "click", `${ctx} #bridge[data-spot]`, function(e){
   const type = "relocate-bridge",
         from = getAttr(this, "data-spot");
   sh.dispatch($wip, {type, details: {from}});
 });
 
-$.on(el, "click", 'body[data-tense="present"][data-ready="true"][data-command-type="place-pilli"][data-command-at~="H6"] div[data-spot="H6"] div.propose, body[data-command-type="place-pilli"][data-command-at~="H8"] div[data-spot="H8"] div.propose, body[data-command-type="place-pilli"][data-command-at~="I7"] div[data-spot="I7"] div.propose, body[data-command-type="place-pilli"][data-command-at~="G7"] div[data-spot="G7"] div.propose', function(e){
+$.on(el, "click", `${ctx}[data-command-type="place-pilli"][data-command-at~="H6"] div[data-spot="H6"] div.propose, ${ctx}[data-command-type="place-pilli"][data-command-at~="H8"] div[data-spot="H8"] div.propose, ${ctx}[data-command-type="place-pilli"][data-command-at~="I7"] div[data-spot="I7"] div.propose, ${ctx}[data-command-type="place-pilli"][data-command-at~="G7"] div[data-spot="G7"] div.propose`, function(e){
   const at = getAttr(this, "data-spot");
   sh.dispatch($story, {type, details: {at}});
 });
 
-$.on(el, "click", 'body[data-tense="present"][data-ready="true"] .moves button[data-type="commit"], body[data-tense="present"][data-ready="true"] .moves button[data-type="pass"]', function(e){
+$.on(el, "click", `${ctx} .moves button[data-type="commit"], ${ctx} .moves button[data-type="pass"]`, function(e){
   const type = dom.attr(this, "data-type");
   sh.dispatch($story, {type});
 });
 
-$.on(el, "click", 'body[data-tense="present"][data-ready="true"] #supplies div.tokens', function(e){
+$.on(el, "click", `${ctx} #supplies div.tokens`, function(e){
   sh.dispatch($story, {type: "bank"});
 });
 
-$.on(el, "click", 'body[data-tense="present"][data-ready="true"][data-command-type="construct-bridge"] div[data-spot]', function(e){
+$.on(el, "click", `${ctx}[data-command-type="construct-bridge"] div[data-spot]`, function(e){
   const type = "construct-bridge",
         at   = getAttr(this, "data-spot");
   sh.dispatch($story, {type, details: {at}});
 });
 
-$.on(el, "click", 'body[data-tense="present"][data-ready="true"] #supplies div.bridges', function(e){
+$.on(el, "click", `${ctx} #supplies div.bridges`, function(e){
   sh.dispatch($wip, {type: "construct-bridge"});
 });
 
-$.on(el, "click", 'body[data-tense="present"][data-ready="true"][data-command-type="construct-canal"][data-command-size="1"] div[data-spot]', function(e){
+$.on(el, "click", `${ctx}[data-command-type="construct-canal"][data-command-size="1"] div[data-spot]`, function(e){
   const type = "construct-canal",
         at   = getAttr(this, "data-spot");
   sh.dispatch($story, {type, details: {at: [at]}});
 });
 
-$.on(el, "click", 'body[data-tense="present"][data-ready="true"][data-command-type="construct-canal"][data-command-size="2"] div[data-spot]', function(e){
+$.on(el, "click", `${ctx}[data-command-type="construct-canal"][data-command-size="2"] div[data-spot]`, function(e){
   const type = "construct-canal",
         at   = _.distinct(_.compact([getAttr(this, "data-command-at"), getAttr(this, "data-spot")])),
         size = parseInt(getAttr(this, "data-command-size"));
@@ -566,7 +568,7 @@ $.on(el, "click", 'body[data-tense="present"][data-ready="true"][data-command-ty
   }
 });
 
-$.on(el, "click", 'body[data-tense="present"][data-ready="true"] #supplies div.canals', function(e){
+$.on(el, "click", `${ctx} #supplies div.canals`, function(e){
   const type = "construct-canal",
         size = parseInt(getAttr(this, "data-size"));
   sh.dispatch($wip, {type, details: {size}});

b1ad3e6598b28f0646d691a4672319eb1c94e83d
Mario T. Lanza <mlanza@doesideas.com>
2023-08-04 19:49:32 -0400

    fix last move

--- sql/functions/last_move.sql
+++ sql/functions/last_move.sql
@@ -10,7 +10,7 @@ return case when _seat_id is null then null else (select id
         and seat_id = _seat_id
         and type = 'committed'
         and seq < (select seq from events where table_id = _table_id and id = _event_id limit 1)
-        order by seq
+        order by seq desc
         limit 1) end;
 
 end;

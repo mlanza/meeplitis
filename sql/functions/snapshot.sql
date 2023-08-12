0e357996be665639f1ff9236cb1f132ec5d64cfb
Mario T. Lanza <mlanza@doesideas.com>
2023-08-12 10:04:43 -0400

    edit snapshot

--- sql/functions/snapshot.sql
+++ sql/functions/snapshot.sql
@@ -1,3 +1,21 @@
+create or replace function snapshot(_table_id varchar, _event_id varchar) returns void
+security definer
+set search_path = public
+as $$
+declare
+  _snapshot jsonb;
+begin
+  select simulate(_table_id, _event_id)
+  into _snapshot;
+
+  raise log '$ took snapshot at % for event %', _table_id, _event_id;
+
+  update events
+  set snapshot = _snapshot
+  where table_id = _table_id and id = _event_id;
+end;
+$$ language plpgsql;
+
 create or replace function snapshot(_table_id varchar, _event_id varchar, _limit int) returns void
 security definer
 set search_path = public
@@ -5,7 +23,6 @@ as $$
 declare
   _event record;
   _seq bigint;
-  _state jsonb;
 begin
   select seq
   from events
@@ -13,14 +30,7 @@ begin
   into _seq;
 
   for _event in select id from events where table_id = _table_id and seq >= _seq order by seq limit _limit loop
-    select simulate(_table_id, _event.id)->'state'
-    into _state;
-
-    raise log '$ snapshot at % for % is %', _table_id, _event.id, _state;
-
-    update events
-    set snapshot = _state
-    where table_id = _table_id and id = _event.id;
+    perform snapshot(_table_id, _event.id);
   end loop;
 end;
 $$ language plpgsql;

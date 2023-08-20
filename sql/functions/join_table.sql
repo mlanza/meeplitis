bb6daa7b688aff9b27ef11833cede4ff7b0e3c2f
Mario T. Lanza <mlanza@doesideas.com>
2023-08-20 18:28:00 -0400

    edit join table

--- sql/functions/join_table.sql
+++ sql/functions/join_table.sql
@@ -9,13 +9,17 @@ begin
 
 _seat_id = (select id from seats where table_id = _table_id and player_id is null order by seat limit 1);
 
-update seats
-set player_id = _player_id,
-    joined_at = now()
-where table_id = _table_id
-and id = _seat_id;
+if _seat_id is not null then
+  update seats
+  set player_id = _player_id,
+      joined_at = now()
+  where table_id = _table_id
+  and id = _seat_id;
 
-raise log '$ player `%` joins seat `%` at table `%`', _player_id, _seat_id, _table_id;
+  raise log '$ player `%` joins seat `%` at table `%`', _player_id, _seat_id, _table_id;
+else
+  raise log '$ player `%` unable to join table `%`', _player_id, _table_id;
+end if;
 
 return _seat_id;
 

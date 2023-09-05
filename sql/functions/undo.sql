47dd1b40e307544932b96bc6e1fee78ff93de54f
Mario T. Lanza <mlanza@doesideas.com>
2023-09-05 17:58:08 -0400

    validate undo

--- sql/functions/undo.sql
+++ sql/functions/undo.sql
@@ -6,11 +6,29 @@ create or replace function undo(_table_id varchar, _event_id varchar)
 as $$
 declare
   _count int;
+  _seat int;
+  _up int[];
+  _allow boolean;
 begin
 
-  select chop
-  from chop(_table_id, _event_id, true)
-  into _count;
+  select up
+  from tables
+  where id = _table_id
+  into _up;
+
+  select seat
+  from seats
+  where table_id = _table_id and player_id = auth.uid()
+  into _seat;
+
+  select array_position(_up, _seat) is not null
+  into _allow;
+
+  if not _allow then
+    raise '$ only % can undo % at table %', _up, _event_id, _table_id;
+  end if;
+
+  select chop from chop(_table_id, _event_id, true) into _count;
 
   return _count;
 

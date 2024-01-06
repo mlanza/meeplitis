fb940dfb92b04ef539acec7f2222ba143fb60ea2
Mario T. Lanza <mlanza@doesideas.com>
2024-01-06 14:51:55 -0500

    upgrade lume

--- _config.js
+++ _config.js
@@ -1,6 +1,7 @@
 import lume from "lume/mod.ts";
 import date from "lume/plugins/date.ts";
 import relative_urls from "lume/plugins/relative_urls.ts";
+import nunjucks from "lume/plugins/nunjucks.ts";
 
 export default lume({
   location: new URL("https://yourmove.cc"),
@@ -14,5 +15,6 @@ export default lume({
   .ignore("README.md")
   .ignore("cf")
   .copy([".html", ".css", ".js", ".pdf", ".gif", ".jpg", ".png", ".webp", ".ico", ".svg", ".json"])
+  .use(nunjucks())
   .use(date())
   .use(relative_urls());

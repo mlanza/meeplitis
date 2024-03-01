717e58f3974a91ec3cee3c10af35f87994df9036
Mario T. Lanza <mlanza@doesideas.com>
2024-02-29 19:19:58 -0500

    update abandons

--- sql/functions/abandons.sql
+++ sql/functions/abandons.sql
@@ -5,18 +5,25 @@ create or replace function abandons()
   language plpgsql
 as $$
 declare
-  _count int;
+  _count int := 0;
+  _stale_table_record record;
+  _stale_tables_cursor cursor for select id, touched_at from tables_since_touched where stale;
 begin
 
-  WITH updated AS (
-    update delinquent_tables
-    set status = 'abandoned'
-    where status = 'started'
-    returning 1
-  )
-  select count(*)
-  FROM updated
-  into _count;
+  open _stale_tables_cursor;
+  loop
+      fetch next from _stale_tables_cursor into _stale_table_record;
+      exit when not found;
+
+      update tables
+      set status = 'abandoned'
+      where id = _stale_table_record.id;
+
+      _count := _count + 1;
+
+      raise log '$ abandoned table % last touched at %', _stale_table_record.id, _stale_table_record.touched_at;
+  end loop;
+  close _stale_tables_cursor;
 
   return _count;
 
--- sql/views/tables_since_touched.sql
+++ sql/views/tables_since_touched.sql
@@ -1,6 +1,8 @@
 create or replace view tables_since_touched as
 select *,
-  now()::timestamp - touched_at::timestamp as since_touched,
-  extract(epoch from now()::timestamp - touched_at::timestamp)::int as seconds_since_touched
+  extract(epoch from now()::timestamp - touched_at::timestamp)::int as seconds_since_touched,
+  extract(day FROM (now()::timestamp - touched_at::timestamp)::int as days_since_touched,
+  touched_at < now() - INTERVAL '30 days' and status = 'started' as stale,
+  touched_at < now() - INTERVAL '30 days' and status = 'finished' and exists(select 1 from events e where e.table_id = tables.id) as plump
 from tables
 order by touched_at, created_at;

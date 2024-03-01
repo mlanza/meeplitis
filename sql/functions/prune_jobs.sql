225a76603f78f1538aa35fde8d4213886f2d2a1a
Mario T. Lanza <mlanza@doesideas.com>
2024-03-01 13:17:50 -0500

    prune jobs

---
+++ sql/functions/prune_jobs.sql
@@ -0,0 +1,26 @@
+create or replace function prune_jobs()
+  returns int
+  security definer
+  set search_path = public
+  language plpgsql
+as $$
+declare
+  _count int := 0;
+begin
+
+  with deletions as (
+    delete from jobs
+    where created_at < now() - interval '30 days'
+    returning *
+  )
+
+  select count(*)
+  from deletions
+  into _count;
+
+  raise log '$ pruned jobs of % rows', _count;
+
+  return _count;
+
+end;
+$$;

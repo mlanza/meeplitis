93d897332ecc5f200193ce64861a2f0c95bf7731
Mario T. Lanza <mlanza@doesideas.com>
2024-03-02 14:25:06 -0500

    remove prune_jobs

--- sql/functions/notify.sql
+++ sql/functions/notify.sql
@@ -7,43 +7,36 @@ as $$
 declare
   _status int = 401; -- no recipients
   _completed boolean;
-  _details jsonb;
   _type varchar;
   _title varchar;
   _slug varchar;
   _table_id varchar;
+  _payload varchar;
   _recipients jsonb;
   _outcome jsonb;
   _response jsonb = null;
   _content json = null;
 begin
 
-  raise log '$ notifying';
-
-  select type, title, slug, table_id, recipients, outcome, completed, details
+  select type, title, slug, table_id, recipients, outcome, completed
   from notices
   where seq = _seq
-  into _type, _title, _slug, _table_id, _recipients, _outcome, _completed, _details;
+  into _type, _title, _slug, _table_id, _recipients, _outcome, _completed;
 
-  raise log '$ notifying % % -> %', _type, _seq, _details;
+  if _recipients is not null and (_retry or not _completed) then
+    select '{"type": "' || _type || '", "title": "' || _title || '", "slug": "' || _slug || '", "table_id": "' || _table_id || '" ,"outcome": ' || coalesce(_outcome, 'null') || ' ,"recipients": ' || _recipients || '}' as payload
+    into _payload;
 
-  if _retry or not _completed then
+    raise log '$ notify % -> %', _seq, _payload;
 
-    raise log '$ notify type %, title %, slug %, table_id %, outcome %, recipients %', _type, _title, _slug, _table_id, _outcome, _recipients;
+    select status, content
+    from http_post('https://notify.workers.yourmove.cc', _payload, 'application/json')
+    into _status, _content;
 
-    if _recipients is not null then
-
-      select status, content
-      from http_post('https://notify.workers.yourmove.cc',
-                    '{"type": "' || _type || '", "title": "' || _title || '", "slug": "' || _slug || '", "table_id": "' || _table_id || '" ,"outcome": ' || coalesce(_outcome, 'null') || ' ,"recipients": ' || _recipients || '}', 'application/json')
-      into _status, _content;
-
-    end if;
-
-    select ('{"status": ' || _status::varchar || ', "recipients": ' || _recipients::varchar || ', "content": ' || _content || '}')::jsonb
+    select ('{"status": ' || _status::varchar || ', "content": ' || _content || '}')::jsonb
     into _response;
 
-    raise log '$ notified % -> %', _status, _response;
+    raise log '$ notified % <- (%) %', _seq, _status, _response;
 
     update notifications
     set completed = (_status = 200),
@@ -53,11 +46,8 @@ begin
     where seq = _seq;
 
     return _response;
-
   else
-
     return null;
-
   end if;
 end;
 $$;

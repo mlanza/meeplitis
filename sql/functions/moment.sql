eebcd3ac7d30b195bad3cf4f894a313b9b583014
Mario T. Lanza <mlanza@doesideas.com>
2024-04-27 12:01:20 -0400

    add moment for debugging

---
+++ sql/functions/moment.sql
@@ -0,0 +1,36 @@
+create or replace function moment(_table_id varchar, _user_id uuid)
+returns jsonb
+security definer
+set search_path = public
+language plpgsql
+as $$
+declare
+_admins int;
+_table jsonb;
+_seated jsonb;
+_evented jsonb;
+begin
+
+select count(*)
+into _admins
+from admins where user_id = _user_id;
+
+if _admins = 0 then
+  raise exception '$ moment for % at table `%` was denied', _user_id, _table_id;
+end if;
+
+raise log '$ moment for % at table `%` was granted', _user_id, _table_id;
+
+select json_build_object('slug', g.slug, 'id', t.id, 'game_id', t.game_id, 'config', t.config)
+into _table
+from tables t
+join games g on g.id = t.game_id
+where t.id = _table_id;
+
+select seated into _seated from seated(_table_id);
+select evented into _evented from evented(_table_id);
+
+return ('{"table": ' || _table::varchar || ', "seated": ' ||  _seated::varchar || ', "evented": ' || _evented::varchar || '}')::jsonb;
+
+end;
+$$;
--- src/debug/index.js
+++ src/debug/index.js
@@ -2,69 +2,75 @@ import _ from "/lib/atomic_/core.js";
 import $ from "/lib/atomic_/reactives.js";
 import g from "/lib/game_.js";
 import supabase from "/lib/supabase.js";
+import session from "/lib/session.js";
 
 const params = new URLSearchParams(document.location.search),
       tableId = params.get('id'),
+      userId = session?.user?.id,
       eventId = document.location.hash.substr(1),
       options = params.get('options')?.split(',') || [];
 
-const [tables, seated, evented] = await Promise.all([
-  supabase.from("tables").select('*,game_id(slug)').eq("id", tableId),
-  supabase.rpc('seated', {_table_id: tableId}),
-  supabase.rpc('evented', {_table_id: tableId})
-]);
+const {data, error, status} = await supabase.rpc('moment', {_table_id: tableId, _user_id: userId});
 
-const table = tables.data[0],
-      seats = _.toArray(_.repeat(_.count(seated.data) || 4, {})),
-      seen = _.toArray(_.range(0, _.count(seats))),
-      slug = table.game_id.slug,
-      config = table.config,
-      {make} = await import(`/games/${slug}/core.js`),
-      simulate = g.simulate(make),
-      effects = _.comp(g.effects, simulate);
+try {
+  if (error) {
+    throw Error(error.message);
+  }
 
-const thru = eventId ? _.maybe(evented.data, _.detectIndex(function({id}){
-  return id === eventId;
-}, _), _.inc) : null;
+  const {table, seated, evented} = data;
+  const seats = _.toArray(_.repeat(_.count(seated) || 4, {})),
+        seen = _.toArray(_.range(0, _.count(seats))),
+        slug = table.slug,
+        config = table.config,
+        {make} = await import(`/games/${slug}/core.js`),
+        simulate = g.simulate(make),
+        effects = _.comp(g.effects, simulate);
 
-const events = thru == null ? evented.data : _.chain(evented.data, _.take(thru, _), _.toArray),
-      held = thru == null ? [] : _.chain(evented.data, _.drop(thru, _), _.toArray);
+  const thru = eventId ? _.maybe(evented, _.detectIndex(function({id}){
+    return id === eventId;
+  }, _), _.inc) : null;
 
-const [curr, prior] = simulate({seats, config, seen}),
-      $game = $.cell(curr);
+  const events = thru == null ? evented : _.chain(evented, _.take(thru, _), _.toArray),
+        held = thru == null ? [] : _.chain(evented, _.drop(thru, _), _.toArray);
 
-$.sub($.hist($game), _.map(g.effects), _.log);
+  const [curr, prior] = simulate({seats, config, seen}),
+        $game = $.cell(curr);
 
-function sim({events = [], commands = [], seat = null} = {}){
-  const snapshot = _.chain($game, _.deref, _.deref);
-  if (_.seq(commands)){
-    if (seat == null) {
-      throw Error("Must specify seat with commands.");
+  $.sub($.hist($game), _.map(g.effects), _.log);
+
+  function sim({events = [], commands = [], seat = null} = {}){
+    const snapshot = _.chain($game, _.deref, _.deref);
+    if (_.seq(commands)){
+      if (seat == null) {
+        throw Error("Must specify seat with commands.");
+      }
+      return effects({seats, config, events, commands, seen: [seat], snapshot});
+    } else {
+      return effects({seats, config, events, commands, seen, snapshot});
     }
-    return effects({seats, config, events, commands, seen: [seat], snapshot});
-  } else {
-    return effects({seats, config, events, commands, seen, snapshot});
   }
-}
 
-function run({events = [], commands = [], seat = null} = {}){
-  if (_.seq(events)){
-    g.batch($game, g.fold, events);
-    _.log("digested", {events});
+  function run({events = [], commands = [], seat = null} = {}){
+    if (_.seq(events)){
+      g.batch($game, g.fold, events);
+      _.log("digested", {events});
+    }
+    if (_.seq(commands)) {
+      const result = sim({commands, seat});
+      _.log("issued", {commands, seat, result});
+      run({events: result.added});
+    }
   }
-  if (_.seq(commands)) {
-    const result = sim({commands, seat});
-    _.log("issued", {commands, seat, result});
-    run({events: result.added});
+
+  function flush(){
+    _.swap($game, _.compact);
+    _.log("flushed");
   }
+
+  run({events});
+
+  //example: run({commands: [{type: "pass"},{type: "commit"}], seat: 1});
+  Object.assign(window, {$game, effects, g, sim, run, held, flush, supabase});
+} catch (ex) {
+  alert(ex);
 }
-
-function flush(){
-  _.swap($game, _.compact);
-  _.log("flushed");
-}
-
-run({events});
-
-//example: run({commands: [{type: "pass"},{type: "commit"}], seat: 1});
-Object.assign(window, {$game, effects, g, sim, run, held, flush, supabase});

89f7889b32447f8bfa775c65f0a639b2498c1d24
Mario T. Lanza <mlanza@doesideas.com>
2024-05-02 17:47:04 -0400

    options const

--- src/libs/supabase.js
+++ src/libs/supabase.js
@@ -1,13 +1,14 @@
 
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 const supabaseUrl = "https://miwfiwpgvfhggfnqtfso.supabase.co",
-      supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODUwMzIzMywiZXhwIjoxOTU0MDc5MjMzfQ.i1l7NNGYF7mChifi8X-Cn_tis-us1Zq1ntyVW-Amdf8";
-export default createClient(supabaseUrl, supabaseKey, {
-  "schema": "public",
-  "headers": {
-    "x-my-custom-header": "meeplitis"
-  },
-  "autoRefreshToken": true,
-  "persistSession": true,
-  "detectSessionInUrl": true
-});
+      supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODUwMzIzMywiZXhwIjoxOTU0MDc5MjMzfQ.i1l7NNGYF7mChifi8X-Cn_tis-us1Zq1ntyVW-Amdf8",
+      options = {
+        "schema": "public",
+        "headers": {
+          "x-my-custom-header": "meeplitis"
+        },
+        "autoRefreshToken": true,
+        "persistSession": true,
+        "detectSessionInUrl": true
+      };
+export default createClient(supabaseUrl, supabaseKey, options);

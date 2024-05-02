90aa423822af45185c8906b57290999745f82264
Mario T. Lanza <mlanza@doesideas.com>
2024-05-02 17:41:29 -0400

    drop remote config

--- src/libs/supabase.js
+++ src/libs/supabase.js
@@ -1,15 +1,13 @@
+
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-function toJSON(resp){
-  return resp.json();
-}
-
-const {supabaseUrl, supabaseKey, options} = await fetch("https://config.workers.meeplitis.com", {
-  mode: "cors",
-  headers: {
-    "accept": "application/json",
-    "content-type": "application/json; charset=UTF-8"
-  }
-}).then(toJSON);
-
-export default createClient(supabaseUrl, supabaseKey, options);
+const supabaseUrl = "https://miwfiwpgvfhggfnqtfso.supabase.co",
+      supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODUwMzIzMywiZXhwIjoxOTU0MDc5MjMzfQ.i1l7NNGYF7mChifi8X-Cn_tis-us1Zq1ntyVW-Amdf8";
+export default createClient(supabaseUrl, supabaseKey, {
+  "schema": "public",
+  "headers": {
+    "x-my-custom-header": "meeplitis"
+  },
+  "autoRefreshToken": true,
+  "persistSession": true,
+  "detectSessionInUrl": true
+});

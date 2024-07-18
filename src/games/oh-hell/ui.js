2261d67acc04695212348ca2556dad96b1a18226
Mario T. Lanza <mlanza@doesideas.com>
2024-07-18 16:19:47 -0400

    simple dom tags

--- src/games/oh-hell/ui.js
+++ src/games/oh-hell/ui.js
@@ -4,7 +4,8 @@ import $ from "/libs/atomic_/shell.js";
 import {session} from "/libs/session.js";
 import {manageTables} from "/components/table/ui.js";
 
-const {div, span, img, a, p, button, submit, form, label, input, radio} = dom.tags(['div', 'span', 'img', 'a', 'p', 'button', 'submit', 'form', 'label', 'input', 'radio', ['input', {type: "radio"}]]);
+const {div, span, img, a, p, button, submit, form, label, input, radio} = dom.tags(['div', 'span', 'img', 'a', 'p', 'button', 'submit', 'form', 'label', 'input']);
+const radio = dom.tag('input', {type: "radio"});
 
 manageTables(function(open, game){
   const el = form({id: "creates"},

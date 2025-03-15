62732b1f500754afe389fa4d3ceb7a7f9394e8d8
Mario T. Lanza <me@mlanza.com>
2025-03-14 20:33:53 -0400

    refine shell

--- src/shell/ui.js
+++ src/shell/ui.js
@@ -5,11 +5,13 @@ import g from "/libs/game_.js";
 import supabase from "/libs/supabase.js";
 import {reg} from "/libs/cmd.js";
 
-reg({supabase, g});
-
 const params = new URLSearchParams(document.location.search),
       _table_id = params.get('id'),
       options = params.get('options')?.split(',') || [];
+
+const moves = _.pipe(_.deref, _.get(_, "frames"), _.last, _.get(_, "game"), g.moves, _.compact, _.toArray);
+
+reg({supabase, g, moves});
 
 try {
   const {data, error, status} = await supabase.rpc('shell', {_table_id});
@@ -44,68 +46,79 @@ try {
       return {...init, init};
     }
 
-    function issue(cmd = {type: null}){
+    function exec(commands = [], seat = null){
       return function(memo){
-        const {type} = cmd;
-        const {seed, frames, idx, at, init} = memo;
-        const max = _.count(frames) - 1;
-        switch(type) {
-          case "reset": {
-            return {...init, init};
-          }
-
-          case "undo": {
-            const min = 0;
-            const _idx = _.max(0, idx - 1);
-            const at = frames[_idx]?.event?.id;
-            return idx <= min ? memo : {seed, frames, idx: _idx, at, init};
-          }
-
-          case "redo": {
-            const max = _.count(frames) - 1;
-            const _idx = _.min(idx + 1, max);
-            const at = frames[_idx]?.event?.id;
-            return idx >= max ? memo : {seed, frames, idx: _idx, at, init};
-          }
-
-          case "flush": {
-            return flush(memo);
-          }
-
-          case "at": {
-            const {id} = cmd;
-            const idx = _.detectIndex(function(frame){
-              return frame?.event?.id == id;
-            }, frames);
-            return {seed, frames, idx, at, init};
-          }
-
-          case "run": {
-            const {seed, frames, idx} = flush(memo);
-            const frame = _.last(frames);
-            const {seat, commands} = cmd;
-            if (seat == null) {
-              throw Error("Must specify seat with commands.");
-            }
-            const {added} = effects({seats, config, loaded: frame.loaded, events: [], commands, seen: [seat], snapshot: frame.state});
-            const addframes = _.toArray(_.drop(1, _.fold(function(memo, event){
-              const {game, loaded} = _.last(memo);
-              const snapshot = _.deref(game);
-              const result = _.chain({seats, config, events: [event], loaded, commands: [], seen, snapshot}, simulate);
-              const [curr] = result;
-              const effs = g.effects(result);
-              return _.conj(memo, {game: curr, loaded: _.conj(loaded, event), ...effs});
-            },[{game: frame.game, loaded: frame.loaded}], tempIds(added))));
-            const _frames = _.toArray(_.concat(frames, addframes));
-            const at = _.chain(_frames, _.last, _.getIn(_, ["event", "id"]));
-            return {seed, frames: _frames, idx: _.count(_frames) - 1, at, init};
-          }
+        const {init} = memo;
+        const {seed, frames, idx} = flush()(memo);
+        const frame = _.last(frames);
+        if (seat == null) {
+          throw Error("Must specify seat with commands.");
         }
+        const {added} = effects({seats, config, loaded: frame.loaded, events: [], commands, seen: [seat], snapshot: frame.state});
+        const addframes = _.toArray(_.drop(1, _.fold(function(memo, event){
+          const {game, loaded} = _.last(memo);
+          const snapshot = _.deref(game);
+          const result = _.chain({seats, config, events: [event], loaded, commands: [], seen, snapshot}, simulate);
+          const [curr] = result;
+          const effs = g.effects(result);
+          return _.conj(memo, {game: curr, loaded: _.conj(loaded, event), ...effs});
+        },[{game: frame.game, loaded: frame.loaded}], tempIds(added))));
+        const _frames = _.toArray(_.concat(frames, addframes));
+        const at = _.chain(_frames, _.last, _.getIn(_, ["event", "id"]));
+        return {seed, frames: _frames, idx: _.count(_frames) - 1, at, init};
       }
     }
 
-    function flush({seed, frames, idx, at, init}){
-      return {seed, frames: _.toArray(_.take(idx + 1, frames)), idx, at, init};
+    function reset(){
+      return function({init}){
+        return {...init, init};
+      }
+    }
+
+    function flush(){
+      return function({seed, frames, idx, at, init}){
+        return {seed, frames: _.toArray(_.take(idx + 1, frames)), idx, at, init};
+      }
+    }
+
+    function undo(){
+      return function(memo){
+        const {seed, frames, idx, init} = memo;
+        const min = 0;
+        const _idx = _.max(0, idx - 1);
+        const at = frames[_idx]?.event?.id;
+        return idx <= min ? memo : {seed, frames, idx: _idx, at, init};
+      }
+    }
+
+    function redo(){
+      return function(memo){
+        const {seed, frames, idx, init} = memo;
+        const max = _.count(frames) - 1;
+        const _idx = _.min(idx + 1, max);
+        const at = frames[_idx]?.event?.id;
+        return idx >= max ? memo : {seed, frames, idx: _idx, at, init};
+      }
+    }
+
+    function at(id){
+      return function({seed, frames, at, init}){
+        const idx = _.detectIndex(function(frame){
+          return frame?.event?.id == id;
+        }, frames);
+        return {seed, frames, idx, at, init};
+      }
+    }
+
+    function help(){
+      $.log(`From the commands pane or the console \`$.swap($state, ...)\`:
+  exec([{type: "pass"},{type: "commit"}], 1))
+  flush()
+  reset()
+  undo()
+  redo()
+  at(id)
+  help()`);
     }
 
     function tempIds(added){
@@ -118,15 +131,11 @@ try {
     const li = dom.tag('li'), pre = dom.tag('pre'), details = dom.tag("details"), summary = dom.tag("summary");
     const $state = $.atom(init({seats, config, seen, evented, hash: location.hash}));
     const state = _.deref($state);
-    const at = _.get(state, "at") ||_.chain(state, _.get(_, "frames"), _.last, _.getIn(_, ["event", "id"]));
+    const pos = _.get(state, "at") ||_.chain(state, _.get(_, "frames"), _.last, _.getIn(_, ["event", "id"]));
     const $hist = $.hist($state);
 
-    if (at) {
-      location.hash = `#${at}`;
-    }
-
-    function exec(cmd){
-      $.swap($state, issue(cmd));
+    if (pos) {
+      location.hash = `#${pos}`;
     }
 
     function when(i, idx){
@@ -183,14 +192,12 @@ try {
     const $hash = dom.hash(document);
     $.sub($hash, function(hash){
       const id = _.replace(hash, "#", "");
-      $.swap($state, issue({type: "at", id}));
+      $.swap($state, at(id));
     });
 
-    function command(){
-      const text = dom.value(dom.sel1("#move"));
-      const command = JSON.parse(text);
-      const seat = _.maybe(dom.value(dom.sel1("#seat")), _.blot, parseInt);
-      return {type: "run", commands: [command], seat};
+    function commands(){
+      const commands = _.chain(dom.sel1("#commands"), dom.value, _.split(_, "\n"), _.mapa(_.trim, _), _.join(", ", _), _.str("_.pipe(", _, ")"));
+      return eval(commands);
     }
 
     $.on(document, "click", "li[id] summary pre", function(e){
@@ -198,15 +205,15 @@ try {
         e.preventDefault();
         const el = _.closest(this, "li[id]"),
               id = dom.attr(el, "id");
-        exec({type: "at", id});
+        $.swap($state, at(id));
       }
     })
 
-    $.on(dom.sel1("#commands"), "keydown", "textarea", function(e){
+    $.on(dom.sel1("#commands"), "keydown", function(e){
       if (e.key == "Enter") {
         e.preventDefault();
         try {
-          exec(command());
+          $.swap($state, commands());
         } catch (ex){
           $.log("error", ex);
           alert(ex?.message || "There was an error.");
@@ -219,35 +226,33 @@ try {
         switch(e.key) {
           case "r":
             e.preventDefault();
-            exec({type: "reset"});
+            $.swap($state, reset());
             break;
 
           case "ArrowLeft":
           case "ArrowUp":
             e.preventDefault();
-            exec({type: "undo"});
+            $.swap($state, undo());
             break;
 
           case "ArrowRight":
           case "ArrowDown":
             e.preventDefault();
-            exec({type: "redo"});
+            $.swap($state, redo());
             break;
 
           case "f":
             e.preventDefault();
-            exec({type: "flush"});
+            $.swap($state, flush());
             break;
 
         }
       }
     });
 
-    $.log("example:");
-    $.log(`  exec({type: "run", commands: [{type: "pass"},{type: "commit"}], seat: 1})`);
-    $.log(`  exec({type: "flush"})`);
+    help();
 
-    reg({$state, exec, issue, simulate, effects});
+    reg({$state, exec, flush, reset, undo, redo, at, help, simulate, effects});
   }
 } catch (ex) {
 }

67bff3962d9756abdda1908a2c9ec0331adc790f
Mario T. Lanza <me@mlanza.com>
2025-10-18 18:34:24 -0400

    drop simulate bundling

--- src/games/up-down/table/kA4/simulate.js
+++
@@ -1,4 +0,0 @@
-import make from "./core.js";
-import _ from "../../../../libs/atomic_/core.js";
-import g from "../../../../libs/game_.js";
-export const simulate = _.comp(g.effects, g.simulate(make));

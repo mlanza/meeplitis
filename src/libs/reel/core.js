25893c2ef4639e5c1a9e0fd76548c519dcb88794
Mario T. Lanza <me@mlanza.com>
2025-11-14 23:08:04 -0500

    save

--- src/libs/reel/core.js
+++ src/libs/reel/core.js
@@ -30,26 +30,31 @@ export function isPosValid(state, pos) {
   return !!_.get(state.perspectives, at);
 }
 
-export function position(state, pos) {
-  if (!isPosValid(state, pos)) {
-    return state; // Do not move if target is not valid
+export function position(pos) {
+  return function(state){
+    if (!isPosValid(state, pos)) {
+      return state; // Do not move if target is not valid
+    }
+    const direction = pos > state.cursor.pos ? FORWARD : BACKWARD;
+    const at = _.nth(state.touches, pos);
+    const cursor = _.assoc(state.cursor, "pos", pos, "at", at, "direction", direction);
+    return _.assoc(state, "cursor", cursor);
   }
-  const direction = pos > state.cursor.pos ? FORWARD : BACKWARD;
-  const at = _.nth(state.touches, pos);
-  const cursor = _.assoc(state.cursor, "pos", pos, "at", at, "direction", direction);
-  return _.assoc(state, "cursor", cursor);
 }
 
-export function resize(state, max){
-  const {cursor, touches} = state;
-  const direction = cursor.max == null || cursor.max < max ? BACKWARD : FORWARD;
-  const pos = _.clamp(cursor.pos == null ? max : cursor.pos, 0, max);
-  const at = _.get(touches, pos);
-  return _.chain(state,
-    _.assocIn(_, ["cursor", "direction"], direction),
-    _.assocIn(_, ["cursor", "at"], at),
-    _.assocIn(_, ["cursor", "max"], max),
-    _.assocIn(_, ["cursor", "pos"], pos));
+export function resize(max){
+  return function(state){
+    const {cursor, touches} = state;
+    const direction = cursor.max == null || cursor.max < max ? BACKWARD : FORWARD;
+    const pos = _.clamp(cursor.pos == null ? max : cursor.pos, 0, max);
+    const at = _.get(touches, pos);
+    return _.chain(state,
+      _.assocIn(_, ["cursor", "direction"], direction),
+      _.assocIn(_, ["cursor", "at"], at),
+      _.assocIn(_, ["cursor", "max"], max),
+      _.assocIn(_, ["cursor", "pos"], pos));
+
+  }
 }
 
 export function addTouches({touches, undoables, last_acting_seat}){
@@ -58,12 +63,11 @@ export function addTouches({touches, undoables, last_acting_seat}){
     const max = _.count(touches) - 1;
     return _.chain(state,
       _.assoc(_, "touches", touches, "undoables", undoables, "last_acting_seat", last_acting_seat),
-      state => resize(state, max),
-      cursor.pos != null ? _.identity : state => position(state, max));
+      resize(max),
+      cursor.pos != null ? _.identity : position(max));
   }
 }
 
 export function addPerspective(at, perspective){
   return _.assocIn(_, ["perspectives", at], perspective);
 }
-

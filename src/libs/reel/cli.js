85f4ab58816a9384111a400a07ea7652805b17e4
Mario T. Lanza <me@mlanza.com>
2025-11-15 18:09:52 -0500

    save

---
+++ src/libs/reel/cli.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env -S deno run --allow-env --allow-read --allow-write --allow-net
+
+import _ from "../atomic_/core.js";
+import $ from "../atomic_/shell.js";
+import { reel } from "./shell.js";
+import { reg } from "../cmd.js";
+import supabase from "../supabase.js";
+import { session } from "../session.js";
+import { keypress } from "https://deno.land/x/cliffy@v0.25.4/keypress/mod.ts";
+import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
+
+function logs(obj){
+  $.log(Deno.inspect(obj, { colors: true, compact: true, depth: Infinity, iterableLimit: Infinity }));
+}
+
+function elideWith(keys, f){
+  const elide = _.includes(keys, _);
+  return function(state){
+    return _.reducekv(function(memo, key, value){
+      return _.assoc(memo, key, elide(key) ? f(value, key) : value);
+    }, {}, state);
+  }
+}
+
+const abbr = _.pipe(
+  elideWith(["touches","perspectives","seated","table"], (value) => `<${_.count(value)} entries>`));
+
+const log = _.comp(logs, abbr);
+
+async function tuiMode($reel) {
+  for await (const event of keypress()) {
+    if (event.key === "q" || event.key === "escape") {
+      Deno.exit();
+    } else if (event.key === "right") {
+      $.dispatch($reel, {type: event.shiftKey ? "present" : "forward"});
+    } else if (event.key === "left") {
+      $.dispatch($reel, {type: event.shiftKey ? "inception" : "backward"});
+    } else if (event.key === "f") {
+      $.dispatch($reel, {type: "ffwd"});
+    } else if (event.key === "l") {
+      $.dispatch($reel, {type: "last-move"});
+    }
+  }
+}
+
+await new Command()
+  .name("reel")
+  .description("Navigate and append to board game timeline")
+  .arguments("<table:string>")
+  .option("--commands <commands:string>", "Commands string")
+  .option("--seat <seat:number>", "Seat number (integer)")
+  .action(async function (opts, tableId){
+    const seat = opts.seat;
+
+    const $reel = reel(tableId, seat);
+
+    reg({$reel});
+
+    $.sub($reel, log);
+
+    await tuiMode($reel);
+
+  })
+  .parse(Deno.args);
---
+++ src/libs/reel/core.js
@@ -0,0 +1,95 @@
+import _ from "../atomic_/core.js";
+
+export const FORWARD = 1;
+export const BACKWARD = -1;
+
+export function init(id, seat) {
+  return {
+    id,
+    seat,
+    touches: null,
+    cursor: {
+      pos: null,
+      at: null,
+      max: null,
+      direction: FORWARD
+    },
+    perspectives: {} // cache
+  };
+}
+
+export function position(n) {
+  return function(state){
+    const max = _.count(state.touches) - 1;
+    const pos = _.clamp(n, 0, max);
+    const direction = pos === 0 || pos > state.cursor.pos ? FORWARD : BACKWARD;
+    const at = _.nth(state.touches, pos);
+    const cursor = _.assoc(state.cursor, "pos", pos, "max", max, "at", at, "direction", direction);
+    return _.assoc(state, "cursor", cursor);
+  }
+}
+
+export function resize(max){
+  return function(state){
+    const {cursor, touches} = state;
+    const direction = cursor.max == null || cursor.max < max ? BACKWARD : FORWARD;
+    const pos = _.clamp(cursor.pos == null ? max : cursor.pos, 0, max);
+    const at = _.get(touches, pos);
+    return _.chain(state,
+      _.assocIn(_, ["cursor", "direction"], direction),
+      _.assocIn(_, ["cursor", "at"], at),
+      _.assocIn(_, ["cursor", "max"], max),
+      _.assocIn(_, ["cursor", "pos"], pos));
+  }
+}
+
+export function addTouches({touches, undoables, last_acting_seat}){
+  const max = _.count(touches) - 1;
+  return function(state){
+    const {cursor} = state;
+    return _.chain(state,
+      _.assoc(_, "touches", touches, "undoables", undoables, "last_acting_seat", last_acting_seat),
+      resize(max),
+      cursor.pos != null ? _.identity : position(max));
+  }
+}
+
+export function addPerspective(at, perspective){
+  return _.assocIn(_, ["perspectives", at], perspective);
+}
+
+export function forward(state) {
+  return _.chain(state, position(state.cursor.pos + 1));
+}
+
+export function backward(state) {
+  return _.chain(state, position(state.cursor.pos - 1));
+}
+
+export function inception(state) {
+  return _.chain(state, position(0));
+}
+
+export function present(state) {
+  return _.chain(state, position(state.cursor.max));
+}
+
+export function at(at) {
+  return function(state){
+    const {cursor, touches} = state;
+    const pos = _.indexOf(touches, at);
+    if (pos === -1) {
+      throw new Error("Unknown moment.");
+    }
+    return _.chain(state, position(pos));
+  }
+}
+
+export function toLastMove(state){
+  const {perspectives, cursor, touches} = state;
+  const {at} = cursor;
+  const perspective = _.get(perspectives, at);
+  const lastMove = _.get(perspective, "last_move");
+  const pos = _.indexOf(touches, lastMove);
+  return _.chain(state, pos === -1 ? _.identity : position(pos));
+}

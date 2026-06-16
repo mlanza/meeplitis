787a7185efd08154e8093bc6f715368541b3ed0c
M. Lanza <me@mlanza.com>
2026-06-16 17:04:23 -0400

    🚀

--- src/libs/reel/gui.js
+++ src/libs/reel/gui.js
@@ -57,6 +57,7 @@ export async function gui(describe, desc, template) {
   const exec = $.dispatch($reel, _);
   const $wip = $.chan($reel, "wip");
   const $error = $.chan($reel, "error");
+  const $queue = $.chan($reel, "queue");
   const $hist = $.hist($reel);
   const $cursor = $.map(_.get(_, "cursor"), $reel);
   const $act = $.map(_.get(_, "act"), $reel);
@@ -295,7 +296,7 @@ export async function gui(describe, desc, template) {
     exec({type: "at", details: {touch}});
   }));
 
-  return $.doto({seat, seats, seated, exec, $reel, $wip, $gui, $table}, reg);
+  return $.doto({seat, seats, seated, exec, $reel, $queue, $wip, $gui, $table}, reg);
 }
 
 export function player(username, avatar_url, seat, ...contents){
--- src/libs/reel/workboard.js
+++ src/libs/reel/workboard.js
@@ -57,8 +57,6 @@ export class Workboard {
   }
 
   _addTicket(id, ticket) {
-    const {key, args, blocking, startedAt} = ticket;
-    console.log({id, key, args, blocking, startedAt});
     $.swap(this.$queue, _.assoc(_, id, ticket));
   }
 
